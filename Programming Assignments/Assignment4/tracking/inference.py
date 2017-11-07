8b1385cc9f5708e26e73d8de81fb888106a472f8
Nikhil Lohia <nikoo28@gmail.com>
2017-11-07 05:42:50 -0700

    Task 5, Assignment 4

--- Programming Assignments/Assignment4/tracking/inference.py
+++ Programming Assignments/Assignment4/tracking/inference.py
@@ -364,8 +364,15 @@ class ParticleFilter(InferenceModule):
         util.sample(Counter object) is a helper method to generate a sample from
         a belief distribution.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # "*** YOUR CODE HERE ***"
+
+        particles = self.particles
+
+        self.particles = []
+        for particle in particles:
+            new_position_distribution = self.getPositionDistribution(self.setGhostPosition(gameState, particle))
+            self.particles.append(util.sample(new_position_distribution))
+
 
     def getBeliefDistribution(self):
         """
